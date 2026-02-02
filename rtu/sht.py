1817695248f1dc0428a473cc65b8daf3404067e2
Shafnaa <saujanashafi2003@gmail.com>
2026-02-02 22:59:50 +0700

    feat(partial): Partial testing

--- rtu/sht.py
+++ rtu/sht.py
@@ -1,11 +1,47 @@
-import asyncio
-import random
+from pymodbus.client import AsyncModbusSerialClient
+
+from config import RTU_BAUD, RTU_PARITY, RTU_PORT, RTU_SHT_STOPBITS
 
 
 async def read_sht(device_id):
-    await asyncio.sleep(0.1)
+    client = AsyncModbusSerialClient(
+        port=RTU_PORT,
+        baudrate=RTU_BAUD,
+        parity=RTU_PARITY,
+        stopbits=RTU_SHT_STOPBITS,
+        bytesize=8,
+        timeout=1,
+    )
 
-    return {
-        "temperature": round(random.uniform(24.0, 32.0), 1),
-        "humidity": round(random.uniform(40.0, 70.0), 1),
-    }
+    data = {}
+
+    await client.connect()
+
+    if not client.connected:
+        print(f"Failed to connect to SHT {device_id}")
+        return data
+
+    try:
+        result = await client.read_holding_registers(
+            0x0001, count=2, device_id=device_id
+        )
+
+        if result.isError():
+            print(f"Error reading SHT {device_id}: {result}")
+
+        else:
+            t = result.registers[0]
+            h = result.registers[1]
+
+            data = {
+                "temperature": t / 10.0,
+                "humidity": h / 10.0,
+            }
+
+    except Exception as e:
+        print(f"Error reading SHT {device_id}: {e}")
+
+    finally:
+        client.close()
+
+    return data
