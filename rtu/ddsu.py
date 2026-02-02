08076776e6824b92de03513fa60e67562882330e
Shafnaa <saujanashafi2003@gmail.com>
2026-02-02 21:45:55 +0700

    feat(partial): Partial testing

--- rtu/ddsu.py
+++ rtu/ddsu.py
@@ -1,22 +1,57 @@
-import asyncio
-import random
+from pymodbus.client import AsyncModbusSerialClient
 
-from helper.simulation import get_simulated_scenario
+from helper.decode import decode_float
+
+from config import RTU_BAUD, RTU_PARITY, RTU_PORT, RTU_DDSU_STOPBITS
 
 
 async def read_ddsu(device_id):
-    # Simulate a small network delay
-    await asyncio.sleep(0.05)
-    sim = get_simulated_scenario()
+    client = AsyncModbusSerialClient(
+        port=RTU_PORT,
+        baudrate=RTU_BAUD,
+        parity=RTU_PARITY,
+        stopbits=RTU_DDSU_STOPBITS,
+        bytesize=8,
+        timeout=1,
+    )
 
-    # Map logic
-    power = sim["p2"] if device_id == 2 else sim["servers"].get(device_id, 0)
+    data = {}
 
-    return {
-        "voltage": round(random.uniform(228.0, 232.0), 2),
-        "current": round(power / 230, 2),
-        "power": round(power, 2),
-        "energy": round(power * 0.1, 2),
-        "power_factor": round(random.uniform(0.95, 0.99), 2),
-        "frequency": round(random.uniform(49.9, 50.1), 2),
-    }
+    await client.connect()
+
+    if not client.connected:
+        print(f"Error reading DDSU {device_id}: {e}")
+        return data
+
+    try:
+        result = await client.read_holding_registers(
+            0x2000, count=16, device_id=device_id
+        )
+
+        if result.isError():
+            print(f"Error reading DDSU {device_id}: {result}")
+
+        else:
+            v = result.registers[0:2]
+            a = result.registers[2:4]
+            p = result.registers[4:6]
+            pr = result.registers[6:8]
+            pf = result.registers[10:12]
+            freq = result.registers[14:16]
+
+            data = {
+                "voltage": decode_float(v),
+                "current": decode_float(a),
+                "power": decode_float(p) * 1000,
+                "reactive_power": decode_float(pr) * 1000,
+                "power_factor": decode_float(pf),
+                "frequency": decode_float(freq),
+            }
+
+    except Exception as e:
+        print(f"Error reading DDSU {device_id}: {e}")
+
+    finally:
+        client.close()
+
+    return data
